2b9a0c559341355f913a6523025b3e91f8e938b3
Mateus Varnier <90485488+tamis2K@users.noreply.github.com>
2024-03-19 14:07:35 -0300

    Update Game.py

--- Bate-Bate/Game.py
+++ Bate-Bate/Game.py
@@ -1,30 +1,32 @@
-import pygame
-import sys
-from MecMovimento import MovendoTexto
+import pygame  
+import sys  
+from MecMovimento import MovendoTexto 
 
+class Game: 
+    def __init__(self):  
+        pygame.init()  
+        self.largura = 800  # Define a largura da tela do jogo.
+        self.altura = 600  # Define a altura da tela do jogo.
+        self.tela = pygame.display.set_mode((self.largura, self.altura))  # Cria a tela do jogo com as dimensões especificadas.
+        pygame.display.set_caption("Bate-Bate")  # Define o título da janela do jogo.
+        self.clock = pygame.time.Clock()  # Cria um objeto clock para controlar a taxa de quadros do jogo.
+        self.MovendoTexto = MovendoTexto("Mateus", 50, self.largura, self.altura)  # Cria um objeto da classe MovendoTexto.
 
-class Game:
-    def __init__(self):
-        pygame.init()
-        self.largura = 800
-        self.altura = 600
-        self.tela = pygame.display.set_mode((self.largura, self.altura))
-        pygame.display.set_caption("Bate-Bate")
-        self.clock = pygame.time.Clock()
-        self.MovendoTexto = MovendoTexto("Mateus", 50, self.largura, self.altura)
+    def run(self):  # Define o método run da classe Game.
+        rodando = True  # Define a variável rodando como True, indicando que o jogo está em execução.
+        while rodando:  # Inicia um loop enquanto o jogo estiver em execução.
+            for evento in pygame.event.get():  # Itera sobre os eventos pygame.
+                if evento.type == pygame.QUIT:  # Verifica se o evento é de fechar a janela.
+                    rodando = False  # Define rodando como False para sair do loop.
 
-    def run(self):
-        rodando = True
-        while rodando:
-            for evento in pygame.event.get():
-                if evento.type == pygame.QUIT:
-                    rodando = False
+            self.MovendoTexto.move()  # Chama o método move do objeto MovendoTexto para atualizar sua posição.
+            self.tela.fill((0, 0, 0))  # Preenche a tela com a cor preta.
+            self.tela.blit(self.MovendoTexto.texto_surf, self.MovendoTexto.rect)  # Desenha o texto na tela.
+            pygame.display.flip()  # Atualiza a tela para exibir as mudanças.
+            self.clock.tick(60)  # Limita a taxa de quadros do jogo para 60 quadros por segundo.
 
-            self.MovendoTexto.move()
-            self.tela.fill((0, 0, 0))
-            self.tela.blit(self.MovendoTexto.texto_surf, self.MovendoTexto.rect)
-            pygame.display.flip()
-            self.clock.tick(360)
+        pygame.quit()  
+        sys.exit()  
 
         pygame.quit()
         sys.exit()
