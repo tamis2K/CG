f9366768846e051f2c251e9bd06a9772a443c995
Mateus Varnier <90485488+tamis2K@users.noreply.github.com>
2024-03-19 14:08:06 -0300

    Update main.py

--- Bate-Bate/main.py
+++ Bate-Bate/main.py
@@ -1,5 +1,5 @@
-from Game import Game
+from Game import Game  
 
-if __name__ == "__main__":
-    game = Game()
-    game.run()
+if __name__ == "__main__":  # Verifica se este módulo está sendo executado como o programa principal.
+    game = Game()  # Cria uma instância da classe Game.
+    game.run()  # Chama o método run() da instância game para iniciar a execução do jogo.
